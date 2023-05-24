3ffb4349b67cec9eefe0a40ec1016cfff99931ed
endreo123 <endreo.leoni@gmail.com>
2023-05-24 10:45:52 -0300

    aula4 - api externa e interface

--- app/src/app.ts
+++ app/src/app.ts
@@ -2,6 +2,8 @@ import { NegociacaoController } from './controllers/negociacao-controller.js';
 
 const controller = new NegociacaoController();
 const form = document.querySelector('.form');
+const botaoImporta = document.querySelector("#botao-importa");
+
 if (form) {
     form.addEventListener('submit', event => {
         event.preventDefault();
@@ -9,4 +11,12 @@ if (form) {
     });
 } else {
     throw Error('Não foi possível inicializar a aplicação. Verifique se o form existe.');
+}
+
+if(!botaoImporta){
+    throw Error ('Botao importa não foi encontrado');
+}else{
+    botaoImporta.addEventListener('click', e => {
+        controller.importaDados();
+    })
 }
